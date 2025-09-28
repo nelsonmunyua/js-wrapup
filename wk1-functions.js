a7cf1719b67144838c47ca0a0323841fc1fc5157
Nelson Munyua <nelsonmunyua8@gmail.com>
2025-09-29 02:30:31 +0300

    wip: functions

---
+++ wk1-functions.js
@@ -0,0 +1,9 @@
+function exerciseByronThePoodle() {
+    console.log("Wake Byron the poodle");
+  console.log("Leash Byron the poodle");
+  console.log("Walk to the park Byron the poodle");
+  console.log("Throw the frisbee for Byron the poodle");
+  console.log("Walk home with Byron the poodle");
+  console.log("Unleash Byron the poodle");
+}
+exerciseByronThePoodle();
