c21dc480ff9671a3d239a20c5122b3390d70c5b3
Nelson Munyua <nelsonmunyua8@gmail.com>
2025-10-04 04:10:13 +0300

    WIP: DOM manipulation

---
+++ dom-elements.js
@@ -0,0 +1,27 @@
+// DOM manipulation
+const element = document.createElement("div");
+document.body.append(element);
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement("li");
+    li.textContent = (i + 1).toString();
+    ul.append(li);
+}
+element.append(ul);
+
+// Add elements to the Dom using innerHTML
+const main = document.getElementById("main");
+main.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
+// change proeperties on the DOM Nodes
+
+function updateDay(day) {
+    let h4 = document.querySelector('h4');
+    h4.textContent = day
+    h4.id = day
+    h4.className = 'title';
+    h4.style.color = 'blue'
+    h4.style.fontSize = '18px'
+}
+updateDay('Teusday');
